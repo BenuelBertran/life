bee508647f350b985c7f8d5f908daf664d40a5f7
Alexandr Kulakov <BenuelBertran@gmail.com>
2019-04-02 12:04:30 +0300

    add static bundle to gh-pages

---
+++ precache-manifest.eb340103980825cfb109575aae3ebb6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e641e760f18469dcd75",
+    "url": "/life/static/js/runtime~main.b653f72e.js"
+  },
+  {
+    "revision": "25ef02b1243a9c52e516",
+    "url": "/life/static/js/main.48b20e70.chunk.js"
+  },
+  {
+    "revision": "ca2e7c271933d380787c",
+    "url": "/life/static/js/2.96af7b63.chunk.js"
+  },
+  {
+    "revision": "25ef02b1243a9c52e516",
+    "url": "/life/static/css/main.b223b8b9.chunk.css"
+  },
+  {
+    "revision": "1ae477f4f74b8fa93fbc13abe1e0a2dc",
+    "url": "/life/index.html"
+  }
+];
